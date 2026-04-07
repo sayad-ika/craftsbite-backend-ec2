efba1944182955cc4b794ac11dff6c4d14ac0e78
sayad_ika <sayad.ibn@craftsmensoftware.com>
2026-04-07 15:54:55 +0600

    feat: update main.tf file to use a s3-statebucket to store the tf state securely

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "trainee-2026-sayad-craftsbite-tfstate"
+    key    = "craftsbite/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
